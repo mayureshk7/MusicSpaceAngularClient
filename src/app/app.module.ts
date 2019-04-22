d5489c327f26fdbb719fe420d450c9f28d71a5ee
mkhendkar <mayuresh300894@gmail.com>
2019-04-21 23:30:57 -0400

    Seperating header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SongsComponent } from './songs/songs.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 
 export function getAuthServiceConfigs() {
@@ -34,7 +35,8 @@ export function getAuthServiceConfigs() {
     ChartsComponent,
     SongsComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
