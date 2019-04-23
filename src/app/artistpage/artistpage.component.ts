3d176794986f9cb2d02e7b67e9cfe22ea0aa51ab
devanshi ganatra <devanshiganatra93@gmail.com>
2019-04-23 00:08:43 -0400

    follow un follow service written , button toggle working too

--- src/app/artistpage/artistpage.component.ts
+++ src/app/artistpage/artistpage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ChartsServiceClient} from '../../services/charts.service.client';
+import {UserServiceClient} from "../../services/user.service.client";
 
 @Component({
   selector: 'app-artistpage',
@@ -9,15 +10,48 @@ import {ChartsServiceClient} from '../../services/charts.service.client';
 
 export class ArtistpageComponent implements OnInit {
 
+/*  textChange : String = 'follow';*/
+  followUnfollow = true;
   artist : any;
-  constructor(private chartsService: ChartsServiceClient) { }
+/*  devanshi: String;*/
+
+
+  constructor(private chartsService: ChartsServiceClient, private userService: UserServiceClient) {
+ /*   this.textChange = "follow";
+    this.devanshi = 'devanshi';*/
+  }
 
   ngOnInit() {
+    /*this.textChange = "follow";*/
     this.chartsService.getArtist()
       .then(artist => {
         console.log("artists", artist)
         this.artist=artist
       })
+  }
+
+
+  followArtist = () => {
+/*    if (this.textChange === 'follow') {
+      this.textChange = 'unfollow'
+      console.log("inside follow")
+      this.userService
+        .followArtist(this.artist.id)
+        .then(response => this.textChange = 'unfollow')
+    }else{
+      console.log('inside unfollow');
+      this.textChange = 'follow'
+      this.userService
+        .unfollowArtist(this.artist.id)
+        .then(response => this.textChange = 'follow')
+
+    }*/
+    this.followUnfollow = !this.followUnfollow;
+    if (this.followUnfollow === true) {
+      this.userService.followArtist(this.artist.id);
+    } else {
+      this.userService.unfollowArtist(this.artist.id);
+    }
   }
 
 }
